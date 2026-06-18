c3856c71237294c253d9ee280a986d9773078d29
Peter Bierma <zintensitydev@gmail.com>
2026-06-18 17:08:31 -0400

    Add a factorial test.

---
+++ tests/test_factorial.py
@@ -0,0 +1,26 @@
+import unittest
+from _test_case import ExampleTestRunner
+
+
+class TestFactorial(ExampleTestRunner, executable_name="unit_factorial"):
+    def _run(self, number: int) -> str:
+        self.compile(args=[str(number)])
+        return self.run_program()
+
+    def test_factorial_values(self) -> None:
+        for input, output in (
+            (0, 1),
+            (1, 1),
+            (2, 2),
+            (3, 6),
+            (4, 24),
+            (5, 120),
+            (6, 720),
+            (7, 5040)
+        ):
+            with self.subTest(input=input, output=output):
+                result = self._run(input).strip("\n")
+                self.assertEqual(result, str(output))
+
+if __name__ == "__main__":
+    unittest.main()
